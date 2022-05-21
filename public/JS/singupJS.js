c0c14c60fbcf8ef446ac5451568daab527dfca62
Yosroz <yos.roz96@gmail.com>
2022-05-21 19:29:41 +0300

    login+singup-001

---
+++ public/JS/singupJS.js
@@ -0,0 +1,7 @@
+const { format } = require("express/lib/response")
+
+const register = document.getElementById('register')
+register.addEventListener('submit', registerUser)
+function registerUser(event) {
+    
+}
