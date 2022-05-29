50ceb16f90b7250dff989055576e7b9f3a5c9825
Asaf Nunu <asaf.nunu@gmail.com>
2022-05-29 12:48:59 +0300

    added files

--- public/JS/indexJS.js
+++ public/JS/indexJS.js
@@ -38,3 +38,29 @@ function loadsignup(){
 
     })
 }
+function loadabout(){
+    fetch("http://localhost:3000/about.html").then(function(response){
+        return response.text()
+    })
+    .then(function(html){
+
+        document.getElementById("renderPage").innerHTML= html;
+
+
+
+
+    })
+}
+function loadcontact(){
+    fetch("http://localhost:3000/contact.html").then(function(response){
+        return response.text()
+    })
+    .then(function(html){
+
+        document.getElementById("renderPage").innerHTML= html;
+
+
+
+
+    })
+}
